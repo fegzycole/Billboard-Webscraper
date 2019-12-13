be4bcf1a4960ec6edf9d8dce468c1b1a7e195d21
Iyara Ferguson <fergusoniyara@gmail.com>
2019-12-13 02:55:05 +0100

    Add main file logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,14 @@
 #!/usr/bin/env ruby
 
 # frozen_string_literal: true
+
+require './lib/scraper'
+require './lib/json_parser'
+
+billboard_scrapper = Scraper.new
+
+array_of_songs = billboard_scrapper.top_songs
+
+json_parser = JsonParser.new(array_of_songs)
+
+puts json_parser.convert_to_json
