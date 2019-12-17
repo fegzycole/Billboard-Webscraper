bc6e52b2f556839cecf576b1233ec6a4117cb631
fegzycole <fergusoniyara@gmail.com>
2019-12-17 20:55:55 +0100

    Add dialog for better user experience

--- bin/main.rb
+++ bin/main.rb
@@ -7,13 +7,17 @@ require_relative '../lib/scraper'
 def scrape_songs
   billboard_scrapper = Scraper.new
 
+  puts 'Welcome to the Billboard scraper, specify a name for your file'
+
+  file_name = gets.chomp
+
   json_array = billboard_scrapper.get_top_songs
 
   if json_array
-    File.open('songs.txt', 'w+') do |f|
+    File.open("#{file_name}.txt", 'w+') do |f|
       json_array.each { |song| f.puts(song) }
     end
-    puts "File successfully generated!"
+    puts "A file #{file_name}.txt has been created in the root directory"
   end
 
 end
