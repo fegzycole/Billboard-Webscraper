42fda4d910f6ef5e9fbd5039df997d0aa40df633
fegzycole <fergusoniyara@gmail.com>
2019-12-17 17:19:27 +0100

    Add exeption handling for the top_songs method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,27 +7,30 @@ require 'open-uri'
 
 class Scraper # :nodoc:
   URL = 'https://www.billboard.com/charts/hot-100'
+
   def initialize
     @list_of_songs = []
   end
 
   def top_songs
-    songs = generate_html.css('button.chart-element__wrapper')
-    songs.each do |link|
-      top_songs = {
-        rank: link.css('span.chart-element__rank__number').text,
-        title: link.css('span.chart-element__information__song').text,
-        artist: link.css('span.chart-element__information__artist').text,
-        weeks_on_count_down: link.css('span.chart-element__meta.text--week').text
-      }
-      @list_of_songs << top_songs
+    begin
+      songs = generate_html.css('button.chart-element__wrapper')
+      songs.each do |link|
+        top_songs = {
+          rank: link.css('span.chart-element__rank__number').text,
+          title: link.css('span.chart-element__information__song').text,
+          artist: link.css('span.chart-element__information__artist').text,
+          weeks_on_count_down: link.css('span.chart-element__meta.text--week').text
+        }
+        @list_of_songs << top_songs
+      end
+      @list_of_songs
+    rescue Exception => error_message
+      error_message
     end
-    @list_of_songs
   end
 
   private
-
-  attr_reader :url
 
   def generate_html
     doc = Nokogiri::HTML(URI.parse(URL).open)
