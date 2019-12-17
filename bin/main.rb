758ca81b4d7cefd0c765924810e2f302a201ea14
fegzycole <fergusoniyara@gmail.com>
2019-12-17 17:23:05 +0100

    Change variable names

--- bin/main.rb
+++ bin/main.rb
@@ -8,7 +8,7 @@ require './lib/json_parser'
 def scrape_songs
   billboard_scrapper = Scraper.new
 
-  array_of_songs = billboard_scrapper.top_songs
+  array_of_songs = billboard_scrapper.get_top_songs
 
   json_parser = JsonParser.new
 
